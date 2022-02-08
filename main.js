525a879c01aba39e6f732090b50bd66bc771cf27
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-02-08 15:04:57 +0200

    get main elements in main page by selector Relates #6

---
+++ main.js
@@ -0,0 +1,4 @@
+const photoContainer=selector('.photo-container');
+const textContainer=selector('.text-container');
+const searchInput=selector('#search-input');
+const searchBtn=selector('.search-btn');
